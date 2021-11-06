fcf39811af3e294f1ee53ff55fe4e19d3ab1a8b8
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-06 13:45:23 -0300

    Embaralhamento de resposta e questao

---
+++ src/functions/embaralharArray.ts
@@ -0,0 +1,5 @@
+export function embaralhar(elemento: any[]): any[] {
+    return elemento.map(valor => ({ valor, aleatorio: Math.random() }))
+        .sort((obj1, obj2) => obj1.aleatorio - obj2.aleatorio)
+        .map(obj => obj.valor)
+}
