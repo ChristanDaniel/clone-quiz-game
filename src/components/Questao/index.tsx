bd8178ec888d4d3d39fcdf1c503d6629a18a4a48
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-08 21:39:38 -0300

    pequenas alterações

--- src/components/Questao/index.tsx
+++ src/components/Questao/index.tsx
@@ -1,4 +1,5 @@
 import QuestaoModel from "../../model/questao";
+import Enuciado from "../Enuciado";
 import styles from "./Questao.module.css"
 
 interface QuestaoProps {
@@ -7,9 +8,10 @@ interface QuestaoProps {
 
 
 export default function Questao(props: QuestaoProps) {
+    const questao = props.valor
     return (
         <div className={styles.questao}>
-
+            <Enuciado texto={questao.enuciado}/>
         </div>
     )
 }
