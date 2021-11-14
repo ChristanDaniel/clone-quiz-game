e45b51ba8e3cf84fedbe6a203d4466512cc5dfb1
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-14 15:46:02 -0300

    css do component Estatistica

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,19 +1,21 @@
-import { useRouter } from "next/dist/client/router"
-import Estatistica from "../src/components/Estatistica"
-import styles from '../styles/resultado.module.css'
+import { useRouter } from "next/dist/client/router";
+import Estatistica from "../src/components/Estatistica";
+import styles from "../styles/resultado.module.css";
 
 export default function Resultado() {
-    const router = useRouter()
+  const router = useRouter();
 
-    const total = +router.query.total
-    const certas = +router.query.certas
-    const percentual = Math.round((certas / total) * 100)
-    return(
-        <div className={styles.resultado}>
-            <h1>Resultado Final</h1>
-            <Estatistica texto="Perguntas" valor={total}/>
-            <Estatistica texto="Certas" valor={certas}/>
-            <Estatistica texto="Percentual" valor={`${percentual}%`}/>
-        </div>
-    )
+  const total = +router.query.total;
+  const certas = +router.query.certas;
+  const percentual = Math.round((certas / total) * 100);
+  return (
+    <div className={styles.resultado}>
+      <h1>Resultado Final</h1>
+      <div className={}>
+        <Estatistica texto="Perguntas" valor={total} />
+        <Estatistica texto="Certas" valor={certas} />
+        <Estatistica texto="Percentual" valor={`${percentual}%`} />
+      </div>
+    </div>
+  );
 }
